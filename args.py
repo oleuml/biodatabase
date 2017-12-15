2489641af6e1d49ebaaca50e78283b715ed98266
oleumlauft <ole.umlauft@gmail.com>
2017-12-15 16:06:41 +0100

    [+] rewrite argparser with argparse lib

--- args.py
+++ args.py
@@ -1,18 +1,97 @@
-from sys import argv
-import re
+import argparse
+import sys
+import os
 
-options = {}
-args = []
+CHR_NUM = 0
+CHR_NAME = 'chr'
 
-i = 1
-while i < len(argv):
-	m = re.search("^-([\w-]+)", argv[i])
-	if m:
-		options[m.group(0)] = True
-	else:
-		options[m.group(0)] = False
+OUT_DIR = None
+RES_DIR = None
+CHR_FILE = None
+SNP_FILE = None
+ANN_FILE = None
 
-	i += 1
+FINDING = False
+CREATING = False
+DATABASE = False
 
-def getOption(name):
-	return options[name] if name in options else None
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--chrnum', nargs=1, metavar='NUM', type=int, choices=range(1,23), help='set number (1-22) of the chromosom', required=True)
+parser.add_argument('-a', '--all', help='run all: --finding, --creating, --database', action='store_true')
+parser.add_argument('--finding', help='find all positions of patterns', action='store_true')
+parser.add_argument('--creating', help='create all indices with corresponding annotations and models', action='store_true')
+parser.add_argument('--database', help='create the database tables', action='store_true')
+
+parser.add_argument('--resource-dir', nargs=1, metavar='PATH',help='set the folder of all resource files: chr<NUM>.fa gencode.v<VERSION>.annotation.gtf')
+parser.add_argument('--chr-file', nargs=1, metavar='FILE', help='set the file path of the chromosom fasta-file. Isn\'t needed if the file in the resource folder.')
+parser.add_argument('--annotation-file', nargs=1, metavar='FILE', help='set the file path of the annotation gtf-file. Isn\'t needed if the file in the resource folder.')
+parser.add_argument('--snp-file', nargs=1, metavar='FILE', help='set the file path of the snp file. Isn\'t needed if the file in the resource folder.')
+parser.add_argument('--output-dir', nargs=1, metavar='PATH', help='set the folder of all generated files')
+
+args = parser.parse_args()
+
+def error(err, num=1):
+    print("Error: {}".format(err))
+    sys.exit(num)
+
+def check_exist(path):
+    if not os.path.exists(path):
+        error("File/Folder not exists {}".format(path))
+
+
+CHR_NUM = args.chrnum[0]
+CHR_NAME = '{}{}'.format(CHR_NAME, CHR_NUM)
+
+#### RESOURCE FILES ####
+if args.resource_dir != None:
+    RES_DIR = args.resource_dir[0]
+    if not RES_DIR.endswith('/'):
+        RES_DIR += '/'
+else:
+    RES_DIR = 'res/'
+check_exist(RES_DIR)
+
+if args.chr_file != None:
+    CHR_FILE = args.chr_file[0]
+else:
+    CHR_FILE = '{}{}.fa'.format(RES_DIR, CHR_NAME)
+check_exist(CHR_FILE)
+
+if args.annotation_file != None:
+    ANN_FILE = args.annotation_file[0]
+else:
+    for file in os.listdir(RES_DIR):
+        if file.startswith('gencode.') and file.endswith('.annotation.gtf'):
+            ANN_FILE = '{}{}'.format(RES_DIR, file)
+check_exist(ANN_FILE)
+
+if args.snp_file != None:
+    SNP_FILE = args.snp_file[0]
+else:
+    for file in os.listdir(RES_DIR):
+        if file.endswith(''): # TODO: add SNP file end
+            SNP_FILE = '{}{}'.format(RES_DIR, file)
+check_exist(SNP_FILE)
+
+#### OUTPUT FOLDER ####
+if args.output_dir != None:
+    OUT_DIR = args.output_dir[0]
+    if not OUT_DIR.endswith('/'):
+        OUT_DIR += '/'
+
+else:
+    OUT_DIR = 'out/'
+if not os.path.exists(OUT_DIR):
+    os.makedirs(OUT_DIR)
+
+
+if args.finding:
+    FINDING = True
+if args.creating:
+    CREATING = True
+if args.database:
+    DATABASE = True
+if args.all:
+    FINDING = True
+    CREATING = True
+    DATABASE = True
--- run.py
+++ run.py
@@ -3,29 +3,12 @@ import os
 import sys
 
 # TODO: file names
-from args import getOption
+from args import *
 from model_to_pos import model_to_pos
 from pos_to_indexed import creating_indexed_models, creating_indexed_annotations
 from regex_annotation import extract_annotations
 from regex_chromosome import finding_patterns
 from db import make_sql_database
-
-FINDING = getOption("-finding") or False
-CREATING = getOption("-creating") or False
-DATABASE = getOption("-database") or False
-
-
-## RECOURCE & DATA PATH ##
-# Path to resources (fasta-files, gtf-files)
-res_path = 'res/'
-# Path to output/created csv-files
-data_path = 'data/'
-
-if not os.path.exists(res_path):
-    print("Directory not exists: {}".format(res_path))
-    sys.exit(1)
-if not os.path.exists(data_path):
-    os.makedirs(data_path)
 
 ################################################################################
 #### FINDING ALL POSITIONS OF PATTERNS ####
@@ -44,69 +27,53 @@ model1_variants = [
     r'CACG(A|C|G)G',
     r'CACGT(A|C|T)']
 
-## CHROMOSOM NAME & FILE PATH ##
-# Chromosom name
-chr_name = 'chr3'
-# Chromosom file path
-chr_file = '{}chr3.fa'.format(res_path)
-
 ## OUTPUT FILES ##
-result_model = '{}model_start_stop.csv'.format(data_path)
-result_variant = '{}variant_start_stop_model.csv'.format(data_path)
+result_model = '{}model_start_stop.csv'.format(OUT_DIR)
+result_variant = '{}variant_start_stop_model.csv'.format(OUT_DIR)
 
 ## HEADERS ##
 header_model = ['model', 'start_pos', 'stop_pos']
 header_variant = ['variant_pos', 'start_pos', 'stop_pos', 'model']
 
 ## ANNOTATIONS FILE PATHS (INPUT/OUTPUT) ##
-annotation_input_file = '{}gencode.v27.annotation.gtf'.format(res_path)
-annotation_output_file = '{}{}_annotations.csv'.format(data_path, chr_name)
-
-## CHECKS EXISTS ##
-if not os.path.exists(chr_file):
-    print("File not exists: {}".format(chr_file))
-    sys.exit(1)
-
-if not os.path.exists(chr_file):
-    print("File not exists: {}".format(annotation_input_file))
-    sys.exit(1)
+annotation_output_file = '{}{}_annotations.csv'.format(OUT_DIR, CHR_NAME)
 
 ## EXECUTION ##
 if FINDING:
-    print("Finding all positions of patterns (models & variants) from \"{}\".".format(chr_file))
+    print("Finding all positions of patterns (models & variants) from \"{}\".".format(CHR_FILE))
     model_to_pos(
-        input_file=chr_file,
+        input_file=CHR_FILE,
         output_file=result_model,
         header=header_model,
         patterns=models)
-    print("Finished extracting positions of model from \"{}\".".format(chr_file))
+    print("Finished extracting positions of model from \"{}\".".format(CHR_FILE))
     model_to_pos(
-        input_file=chr_file,
+        input_file=CHR_FILE,
         output_file=result_variant,
         header=header_variant,
         patterns=model1_variants)
-    print("Finished extracting positions of variants from \"{}\".".format(chr_file))
+    print("Finished extracting positions of variants from \"{}\".".format(CHR_FILE))
 
-    print("Extracting annotations from \"{}\".".format(annotation_input_file))
+    print("Extracting annotations from \"{}\".".format(ANN_FILE))
     extract_annotations(
-        input_file=annotation_input_file,
+        input_file=ANN_FILE,
         output_file=annotation_output_file,
-        chr_n=chr_name)
-    print("Finished extracting annotations from \"{}\".".format(annotation_input_file))
+        chr_n=CHR_NAME)
+    print("Finished extracting annotations from \"{}\".".format(ANN_FILE))
 
 ################################################################################
 #### CREATING ALL INDICES WITH CORRESPONDING ANNOTATIONS & MODELS ####
 
 ## INPUT FILES ##
 all_models = [
-    '{}model1_start_stop.csv'.format(data_path),
-    '{}model2_start_stop.csv'.format(data_path),
-    '{}model3_start_stop.csv'.format(data_path)]
-annotation_file = '{}{}_annotations.csv'.format(data_path, chr_name)
+    '{}model1_start_stop.csv'.format(OUT_DIR),
+    '{}model2_start_stop.csv'.format(OUT_DIR),
+    '{}model3_start_stop.csv'.format(OUT_DIR)]
+annotation_file = '{}{}_annotations.csv'.format(OUT_DIR, CHR_NAME)
 
 ## OUTPUT FILES ##
-result_file_model = '{}pos_to_model.csv'.format(data_path)
-result_file_region = '{}pos_to_region.csv'.format(data_path)
+result_file_model = '{}pos_to_model.csv'.format(OUT_DIR)
+result_file_region = '{}pos_to_region.csv'.format(OUT_DIR)
 
 ## HEADERS ##
 header_model = ['pos', 'model']
@@ -117,8 +84,8 @@ if CREATING:
     print("Creating all indices with corresponding models.")
     #TODO: FINDING PATTERNS multiple files???
     finding_patterns(
-        input_file=chr_file,
-        output_path=data_path)
+        input_file=CHR_FILE,
+        output_path=OUT_DIR)
     #TODO: Input files
     creating_indexed_models(
         input_files=all_models,
@@ -136,7 +103,7 @@ if CREATING:
 #### CREATING DATABASE TABLES ####
 
 if DATABASE:
-    database = '{}.db'.format(chr_name)
+    database = '{}.db'.format(CHR_NAME)
     database_tables = [
         result_file_model,
         #result_file_region, # TODO: overlarge file
