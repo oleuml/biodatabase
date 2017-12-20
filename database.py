41914da37fc7a75a54a47e5afab1010a5fd7d365
Dorothea Mueller <dorothea.mueller01@stud.uni-goettingen.de>
2017-12-20 19:35:31 +0100

    Fix indices.

--- database.py
+++ database.py
@@ -9,8 +9,13 @@ def make_sql_database(db_name, csv_files):
 	for f in tqdm(csv_files):
 		name = f.split("/")[1].split(".")[0];
 		data = pd.read_csv(f)
-		data.to_sql(name=name, con=conn, if_exists = 'replace', index=True)
+		if(name.startswith("chr")):
+			data.set_index(['start', 'stop'], inplace = True)
+			data.to_sql(name=name, con=conn, if_exists = 'replace', index=True)
+		else:
+			data.to_sql(name=name, con=conn, if_exists = 'replace', index=False)
 		conn.commit()
+
 	c.close()
 	conn.close()
 	
